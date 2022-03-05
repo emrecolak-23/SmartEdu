a94c1040125d6f83a070e714ae0203c5e4690d13
Emre Çolak <colakkemre@gmail.com>
2022-03-05 22:11:01 +0300

    Navigation Links Updated

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require("express");
-
 const app = express();
+
+// Template Engine
+app.set('view engine', 'ejs');
+
+// Middlewares
+app.use(express.static('public'));
 
 // Route
 app.get("/", (req, res) => {
-  res.send("Index Sayfası");
+  res.status(200).render('index',{
+    page_name:'index'
+  });
+});
+
+app.get("/about", (req,res)=>{
+  res.status(200).render('about',{
+    page_name:'about'
+  });
 });
 
 // Declare Port
